3d2e8e6302083dea3f8535a1443acb043860e602
Kiritchell <kiritchell@gmail.com>
2022-03-12 12:09:51 -0600

    index.html

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+ //comment
+ let  myFirstName = "Kirk";
+ let myLastName = "Mitchell";
+ let age = 27;
+ let faveColor = "red";
+
+ console.log(myFirstName + myLastName + "is" + age + "and his favorite color is" + faveColor)
+
+let person = {
+    name: "Kirk",
+    age: 27,
+    faveColor: "red",
+}
+
+console.log(person)
+
+let selectedColors = ['red', 'blue', 'green'];
+selectedColors[3] = "gold";
+console.log(selectedColors)
+console.log(selectedColors.length)
+
+function greet(name, lastname){
+console.log('hello' + myFirstName + myLastName);
+}
+greet(myFirstName, + myLastName);
+
