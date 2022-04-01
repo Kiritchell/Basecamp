adfbad20832142704d6876f41f09935d0c3ca600
Kiritchell <kiritchell@gmail.com>
2022-04-01 15:57:21 -0500

    ../JS/index.html

--- JS/index.js
+++ JS/index.js
@@ -1,26 +1,6 @@
  //comment
- let  myFirstName = "Kirk";
- let myLastName = "Mitchell";
- let age = 27;
- let faveColor = "red";
 
- console.log(myFirstName + myLastName + "is" + age + "and his favorite color is" + faveColor)
-
-let person = {
-    name: "Kirk",
-    age: 27,
-    faveColor: "red",
-}
-
-console.log(person)
-
-let selectedColors = ['red', 'blue', 'green'];
-selectedColors[3] = "gold";
-console.log(selectedColors)
-console.log(selectedColors.length)
-
-function greet(name, lastname){
-console.log('hello' + myFirstName + myLastName);
-}
-greet(myFirstName, + myLastName);
+ function tithetotal(parseInt('a'),y){return x * .1}
+ let answertithe = tithetotal(1500)
+ document.getElementById('tithe').innerHTML=answertithe;
 
