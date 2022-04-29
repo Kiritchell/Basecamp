976f0ccf43802381fef67715dbb9a5754866b301
Kiritchell <kiritchell@gmail.com>
2022-04-29 18:41:51 -0500

    JS/index.html

--- JS/index.js
+++ JS/index.js
@@ -1,6 +1,22 @@
  //comment
+ var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+        alert('hello my dude');
+     localStorage.setItem('alerted','yes');
+    }
 
- function tithetotal(parseInt('a'),y){return x * .1}
- let answertithe = tithetotal(1500)
- document.getElementById('tithe').innerHTML=answertithe;
+ var userName=('Kirk');
+ document.getElementById("userName").innerHTML = userName;
+//document.write('hello' + userName);//
 
+function getInputValue(){
+    var inputVal = document.getElementById("myInput").value;
+//__________
+  let tithe = myTithe();
+  function myTithe(inputVal) {
+    return inputVal * .10;             
+  }
+
+  document.getElementById('myTitheAmount').innerHTML = tithe;
+  //
+}
