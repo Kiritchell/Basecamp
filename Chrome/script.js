67a8d47bb0568adbd529cf05e69ce4ae243f3186
Kiritchell <kiritchell@gmail.com>
2022-05-06 12:27:58 -0500

    index.html

--- Chrome/script.js
+++ Chrome/script.js
@@ -10,7 +10,27 @@ inputBtn.addEventListener("click", function(){
 function renderLeads(){
     let listItems = " "
     for (let i=0; i<myLeads.length; i++){
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+        <li>
+            <a target='_blank' href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>
+    `
+        inputEl.value = ""
     }
     ulEl.innerHTML = listItems
 }
+
+const recipient = "James"
+const sender = "Kirk"
+
+const email = `
+Hey ${recipient}!
+
+How is it going? 
+
+Cheers ${sender}
+`
+
+console.log(email)
