5169203b0e4e73efef22a304dc908111f4474739
Kiritchell <kiritchell@gmail.com>
2023-04-16 16:43:29 -0500

    --all

--- scrimba module 5/cookie consent/script.js
+++ scrimba module 5/cookie consent/script.js
@@ -1,5 +1,10 @@
 const close = document.querySelector('#close')
 const modal = document.querySelector('#modal')
+const modalText = document.querySelector('.modal-text')
+const nameInput = document.querySelector('#name-input')
+const info = document.querySelector('.info')
+const info2=document.querySelector('.info2')
+const buttonDiv = document.querySelector('.button-div-2')
 
 close.addEventListener('click', function(){
     modal.style.display = 'none'
@@ -7,9 +12,22 @@ close.addEventListener('click', function(){
 
 setTimeout(function(){modal.style.display='block'},500)
 
+
+
 const loginForm = document.querySelector('#login-form')
 
 loginForm.addEventListener('submit', function(e){
-e.preventDefault()
-console.log('form submitted')
+    e.preventDefault()
+    console.log('form submitted')
+    modalText.textContent = 'Spam requested! Good luck getting rid of it! 👻'
+    setTimeout(function(){
+        modalText.textContent= 'Sold data to Russia'
+    }, 1000)
+        setTimeout(function(){
+            modalText.innerHTML='You suck'
+            info.style.display='none'
+            info2.style.display='none'
+            buttonDiv.style.display='none'
+        },2000)
+
 })
