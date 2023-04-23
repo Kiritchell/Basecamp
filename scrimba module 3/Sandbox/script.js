af2ffa004a553d669d27680c26e702d318a76609
Kiritchell <kiritchell@gmail.com>
2023-04-22 21:00:33 -0500

    --all

--- scrimba module 3/Sandbox/script.js
+++ scrimba module 3/Sandbox/script.js
@@ -1,3 +1,7 @@
-function hello(){
-    return "Hello"
-}
+const modal = document.querySelector('#modal')
+
+modal.style.display="none"
+
+setTimeout(function(){
+    modal.style.display="block"
+},4000)
