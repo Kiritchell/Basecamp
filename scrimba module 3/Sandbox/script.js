9fa4a461b99d42a7b72ad3262acb39220081fe8c
Kiritchell <kiritchell@gmail.com>
2023-04-22 21:22:45 -0500

    --all

--- scrimba module 3/Sandbox/script.js
+++ scrimba module 3/Sandbox/script.js
@@ -1,7 +1,23 @@
 const modal = document.querySelector('#modal')
+const form = document.querySelector('#form')
 
 modal.style.display="none"
 
 setTimeout(function(){
     modal.style.display="block"
-},4000)
+},0)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+const dataFromForm = new FormData(form)
+
+const formName = dataFromForm.get('Name')
+const formEmail = dataFromForm.get('Email')
+const formPassword = dataFromForm.get('Password')
+
+
+console.log(formName)
+console.log(formEmail)
+console.log(formPassword)
+})
