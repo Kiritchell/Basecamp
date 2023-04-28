116ca0188aba6425cadd6c47d2f97e47d9812d9d
Kiritchell <kiritchell@gmail.com>
2023-04-27 20:16:31 -0500

    --all

--- scrimba module 5/cookie consent/gif.js
+++ scrimba module 5/cookie consent/gif.js
@@ -171,3 +171,36 @@ const catsData = [
         alt: "A cat looking sad",
     },
 ]
+
+
+
+
+
+function getEmotionsArray(cats){
+    const newEmotions = []
+        for (let cat of cats){
+            for (let emotion of cat.emotionTags){
+                newEmotions.push(emotion)
+            }
+        }
+   return newEmotions
+}
+
+
+function renderEmotionsRadios(cats){
+    const emotions = getEmotionsArray(cats)
+    console.log(emotions )
+
+    /*
+
+    2. Inside renderEmotionsRadios, set up a const called
+       "emotions" and set it equals to whatever is returned
+       by calling getEmotionsArray, passing in "cats" as an
+       argument.
+    3. For now, renderEmotionsRadios should just log out
+       "emotions".
+    4. Call renderEmotionsRadios passing in catsData.
+    */
+    }
+
+    renderEmotionsRadios(catsData)
