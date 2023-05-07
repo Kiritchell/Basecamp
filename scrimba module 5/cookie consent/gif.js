bd0ef850cb8ce37f089313946a86d407ec01c9ad
Kiritchell <kiritchell@gmail.com>
2023-05-07 16:19:17 -0500

    --all

--- scrimba module 5/cookie consent/gif.js
+++ scrimba module 5/cookie consent/gif.js
@@ -59,26 +59,113 @@ activeEmotion.parentElement.classList.add('highlight');
 
 
 const getImage=document.querySelector('#meme-submit')
+const isGif = document.querySelector('#gif-checkbox')
 
 getImage.addEventListener('click', getMatchingCatsArray)
 
 
-function getMatchingCatsArray(e){
 
-if(document.querySelector('input[type="radio"]:checked')){
-    const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
-    console.log(selectedEmotion)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+Challenge:
+1. Change the .filter() method's function so it returns an
+   array that only has GIFs if the 'GIFs only' option is
+   checked. If the 'GIFs only' option is not checked, it
+   should return an array of all matches as it does now.
+*/
+
+
+
+function getMatchingCatsArray(e){
+    if(document.querySelector('input[type="radio"]:checked')){
+        const selectedEmotion = document.querySelector('input[type="radio"]:checked').value
+
+        const matchingCatEmotion = catsData.filter(function(catSpecial){
+            if (isGif.checked){
+                console.log('true')
+               return  catSpecial.emotionTags.includes(selectedEmotion) && catSpecial.isGif
+
+            }
+            else{
+                return  catSpecial.emotionTags.includes(selectedEmotion) }
+
+        })
+
+       return matchingCatEmotion
+
 }else(console.log('Select an Emotion'))
 
-}
 
-const isGif = document.querySelector('#gif-checkbox')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 getImage.addEventListener('click', function(){
     if (isGif.checked && document.querySelector('input[type="radio"]:checked') ){
+
         console.log('show gif')
     }
     else{
         console.log('pick yo emotion to get gif')
     }
 })
+}
