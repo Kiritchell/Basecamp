debfe1b4bbc1334b9a21f130a5653ce944086a66
Kiritchell <kiritchell@gmail.com>
2023-05-23 22:08:31 -0500

    --all

---
+++ Camp Name Generator/script.js
@@ -0,0 +1,35 @@
+
+
+// const body = document.querySelector('body')
+// const SelectedName =  document.querySelector('#SelectedName')
+
+
+
+
+
+
+// function pickName(){
+//     let picked = Math.floor(Math.random()*(randName.length-1))
+//     console.log(picked)
+//     console.log(randName[picked].theirName)
+//     let color = randName[picked].color
+//     console.log(randName[picked].color)
+//         if (color==='blue'){
+//             body.style.backgroundColor='blue'
+//             SelectedName.style.color="white"
+//         }
+//         if (color==='red'){
+//             body.style.backgroundColor='red'
+//             SelectedName.style.color="white"
+//         }
+//         if (color==='white'){
+//             body.style.backgroundColor='white'
+//             SelectedName.style.color="black"
+//         }
+//         if (color==='orange'){
+//             body.style.backgroundColor='orange'
+//             SelectedName.style.color="white"
+//         }
+//     SelectedName.innerHTML = randName[picked].theirName
+//     //  return Math.floor(Math.random() * (max - min) + min)
+// }
